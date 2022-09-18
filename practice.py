6ee021f0ae22bb911cd1f4fe6df71179afe3b1ef
achiachiachi <113848464+achiachiachi@users.noreply.github.com>
2022-09-18 20:13:50 +0400

    Delete practice.py

--- practice.py
+++
@@ -1,6 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-word =  generate_password_hash('password')
-print(word)
-var = check_password_hash(word, 'password')
-print(var)
