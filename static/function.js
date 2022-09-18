24e641cd45191efb1b51049683becc8a0a44aacf
achi <achishonia123@gmail.com>
2022-09-18 20:04:44 +0400

    Initial Commit

---
+++ static/function.js
@@ -0,0 +1,5 @@
+function load(){
+    document.getElementById("boxe").style.display = "none";
+    document.getElementById("boxee").style.display = "block";
+    document.getElementById("boxee").style.display = "flex";
+}
